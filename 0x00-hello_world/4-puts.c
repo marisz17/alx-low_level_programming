6eb24fec530a7f2dbfee125b9363b33dff0e1935
marisz17 <marisaninc@gmail.com>
2022-12-08 15:37:31 +0200

    Puts added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- *Use puts
+ *main - uses puts
  *
+ *return: number to be returned
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
