b64d2ea20d5933b3d316490c770a690e14db4fa7
marisz17 <marisaninc@gmail.com>
2022-12-09 15:53:33 +0200

    Numbers added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - prints lowcase alpha
+ *
+ *Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10; i += 1)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
