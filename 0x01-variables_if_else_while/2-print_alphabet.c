6e334f9672f3955322af453b793a517c20107920
marisz17 <marisaninc@gmail.com>
2022-12-09 14:16:10 +0200

    Print alphabet added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - prints lowcase alpha
+ *
+ *Return: zero
+ */
+int main(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z'; i+=1)
+	{
+		putchar(i);
+	}
+	return (0);
+}
