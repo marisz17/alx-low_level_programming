d90e61a717f88da17ce9d2cac8ffda57e3e651a6
marisz17 <marisaninc@gmail.com>
2022-12-09 16:42:15 +0200

    Reverse alpha added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - prints lowcase alpha
+ *
+ *Return: zero
+ */
+int main(void)
+{
+	char i;
+	char x = '\n';
+
+	for (i = 'z' ; i <= 'a'; i -= 1)
+	{
+		putchar(i);
+	}
+	putchar(x);
+	return (0);
+}
