c0575a55bbacfc5560cf3d2889428abfdd1a2849
marisz17 <marisaninc@gmail.com>
2022-12-12 13:27:37 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,33 +8,33 @@
 
 int main(void)
 {
-  int i;
-  int j;
-  int k;
+	int i;
+	int j;
+	int k;
 
-  for (i = 48 ; i < 58 ; i++)
-  {
-    for (j = 48 ; j < 58 ; j++)
-    {
-      for (k = 48 ; k < 58 ; k++)
-      {
-        if (i < j)
-        {
-          if ( j < k)
-          {
-            putchar(i);
-            putchar(j);
-            putchar(k);
-            if (i < 55)
-            {
-              putchar(44);
-              putchar(32);
-            }
-          }
-        }
-      }
-    }
-  }
-    putchar(10);
-    return (0);
+	for (i = 48 ; i < 58 ; i++)
+	{
+		for (j = 48 ; j < 58 ; j++)
+		{
+			for (k = 48 ; k < 58 ; k++)
+			{
+				if (i < j)
+				{
+					if ( j < k)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(k);
+						if (i < 55)
+						{
+							putchar(44);
+							putchar(32);
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
 }
