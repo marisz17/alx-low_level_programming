611c8356edc029509ebea62b53e54e4052d6aebc
marisz17 <117833301+marisz17@users.noreply.github.com>
2022-12-12 11:19:26 +0200

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ *main - three digit combination
+ *
+ *Return: zero
+ */
+
+int main(void)
+{
+  int i;
+  int j;
+  int k;
+
+  for (i = 48 ; i < 58 ; i++)
+  {
+    for (j = 48 ; j < 58 ; i++)
+    {
+      for (k = 48 ; k < 58 ; k++)
+      {
+        if (i < j && j < k)
+        {
+          putchar(i);
+          putchar(j);
+          putchar(k);
+          if (i < 55)
+          {
+            putchar(44);
+            putchar(32);
+          }
+        }
+      }
+    }
+    putchar(10);
+    return (0);
