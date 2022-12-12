44477ad862612a02e67b7c69771cb23e5aa61b9d
marisz17 <marisaninc@gmail.com>
2022-12-12 00:52:38 -0800

    two digit combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,9 @@ int main(void)
   int i;
   int j;
 
-  for (i = 0 ; i < 58 ; i++)
+  for (i = 48 ; i < 58 ; i++)
     {
-      for (j = 0 ; j < 58 ; j++)
+      for (j = 48 ; j < 58 ; j++)
 	{
 	  if (i < j)
 	    {
