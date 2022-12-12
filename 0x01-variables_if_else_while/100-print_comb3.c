a0342cf02446819877fdd757e6074fcca49ceb84
marisz17 <117833301+marisz17@users.noreply.github.com>
2022-12-12 11:04:55 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,25 +8,24 @@
 
 int main(void)
 {
-  int i;
-  int j;
-
-  for (i = 48 ; i < 58 ; i++)
-    {
-      for (j = 48 ; j < 58 ; j++)
+	int i;
+	int j;
+	for (i = 48 ; i < 58 ; i++)
 	{
-	  if (i < j)
-	    {
-	      putchar(i);
-	      putchar(j);
-	      if (i < 56)
+		for (j = 48 ; j < 58 ; j++)
 		{
-		  putchar(44);
-		  putchar(32);
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i < 56)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
 		}
-	    }
 	}
-    }
   putchar(10);
   return (0);
 }
