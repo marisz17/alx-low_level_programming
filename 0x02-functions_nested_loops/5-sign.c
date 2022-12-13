68cf686b36d729cb281b087f38b10e01c4d10a48
marisz17 <marisaninc@gmail.com>
2022-12-13 22:44:38 +0200

    sign added

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,12 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(49);
+		return (1);
 	} else if (n == 0)
 	{
-		_putchar(48);
+		return (0);
 	} else
 	{
-		_putchar(45);
-		_putchar(49);
+		return (-1);
 	}
-	return (0);
 }
