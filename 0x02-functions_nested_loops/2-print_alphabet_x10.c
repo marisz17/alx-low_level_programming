753ebe22488693ace44ec0e903596a46841c75e8
marisz17 <marisaninc@gmail.com>
2022-12-13 17:15:06 +0200

    alphax10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints lowcase alphax10
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9; i += 1)
+	{
+		print_alphabet();
+	}
+}
