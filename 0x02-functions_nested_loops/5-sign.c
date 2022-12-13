3143c197c4af6147e7e74305f40e05a658379de5
marisz17 <marisaninc@gmail.com>
2022-12-13 22:30:22 +0200

    sign added

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_sign - print sign
+ *@n: number
+ *Return: zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(49);
+	} else if (n == 0)
+	{
+		_putchar(48);
+	} else if (n < 0)
+	{
+		_putchar(45);
+		_putchar(49);
+	}
+}
