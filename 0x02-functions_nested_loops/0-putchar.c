d92e0c71b706dcbb6747b69f6f87211acf9ad3ba
marisz17 <marisaninc@gmail.com>
2022-12-13 15:41:28 +0200

    header file and putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ *main - prints _putchar from function _putchar()
+ *
+ *Return: zero
+ */
+
+char main(void)
+{
+	char x = "_putchar";
+	char y;
+
+	y = _putchar(x);
+	return y;
+}
