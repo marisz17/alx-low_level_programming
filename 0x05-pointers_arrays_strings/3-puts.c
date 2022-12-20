565cb2762162b8b5209c7d8976d02b12ec5ddabe
marisz17 <marisaninc@gmail.com>
2022-12-20 19:05:48 +0200

    puts added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_puts - length of string
+ *@str: string input
+ *
+ *Return: void
+ */
+void _puts(char *str)
+{
+	int i = 1;
+	int j = 0;
+	int m;
+	char k = str[0];
+
+	while (k != '\0')
+	{
+		j++;
+		k = str[i++];
+	}
+	for (m = 0 ; m < j ; m++)
+	{
+		_putchar(str[m]);
+	}
+	_putchar(10);
+}
