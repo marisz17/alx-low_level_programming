5c2801b5d7fbb279ae6e40a8f156c284a7034b53
marisz17 <marisaninc@gmail.com>
2022-12-20 19:17:36 +0200

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_rev - string in reverse
+ *@str: string input
+ *
+ *Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 1;
+	int j = 0;
+	int m;
+	char k = s[0];
+
+	while (k != '\0')
+	{
+		j++;
+		k = s[i++];
+	}
+	for (m = j-1 ; m >= 0 ; m--)
+	{
+		_putchar(s[m]);
+	}
+	_putchar(10);
+}
