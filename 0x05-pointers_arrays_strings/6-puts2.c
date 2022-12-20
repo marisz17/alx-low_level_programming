51f9eca6fc9735092528bd2646f913c83f17be78
marisz17 <marisaninc@gmail.com>
2022-12-20 22:16:40 +0200

    puts added

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *puts2 - prints string every other number
+ *@str: string input
+ *
+ *Return: void
+ */
+void puts2(char *str)
+{
+	int i = 1;
+	int j = 0;
+	int m;
+	char k = str[0];
+
+	while (k != '\0')
+	{
+		j++;
+		k = str[i++];
+	}
+	for (m = 0 ; m < j ; m+=2)
+	{
+		_putchar(str[m]);
+	}
+	_putchar(10);
+}
